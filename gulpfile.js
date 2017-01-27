06373b2850684c553b65a02612af16b45ab921d8
Jordan <jordanmoconnor@gmail.com>
2017-01-27 11:27:25 -0500

    optimize images

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,23 @@
 const gulp = require('gulp');
+var jimp = require("gulp-jimp-resize");
 const imagemin = require('gulp-imagemin');
 
-gulp.task('images', function() {
+gulp.task('imagemin', function() {
     gulp.src('_uploads/*')
         .pipe(imagemin())
         .pipe(gulp.dest('_uploads'))
+});
+
+gulp.task('image-resize', function() {
+    return gulp.src(
+        '_uploads/*.{png,jpg,bmp}'
+        )
+    .pipe(jimp({
+        sizes: [
+            {"width": 720}
+        ]
+    }))
+    .pipe(gulp.dest('_uploads/'));
 });
 
 gulp.task('default', function() {
