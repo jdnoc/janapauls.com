63d4fcfa8996c42483dd4f68fa38308110965168
Jordan <jordanmoconnor@gmail.com>
2017-01-27 10:28:42 -0500

    edit image sizes

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,17 @@
+var jimp = require("gulp-jimp-resize");
 var gulp = require('gulp');
+ 
+gulp.task('images', function() {
+    return gulp.src(
+        '_uploads/*.{png,jpg,bmp}'
+        )
+    .pipe(jimp({
+        sizes: [
+            {"width": 960},
+        ]
+    }))
+    .pipe(gulp.dest('_uploads/'));
+});
 
 gulp.task('default', function() {
   // place code for your default task here
