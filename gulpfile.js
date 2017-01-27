78b685c4f2bcaa77bf9caae62b16ac683fd68447
Jordan <jordanmoconnor@gmail.com>
2017-01-27 11:12:52 -0500

    add imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,11 @@
-var gulp = require('gulp');
+const gulp = require('gulp');
+const imagemin = require('gulp-imagemin');
+
+gulp.task('images', function() {
+    gulp.src('_uploads/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('_uploads'))
+});
 
 gulp.task('default', function() {
   // place code for your default task here
