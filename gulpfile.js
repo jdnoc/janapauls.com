3bda6af6ce6c2c2edb6f92bf65b18cdb07df21ef
Jordan <jordanmoconnor@gmail.com>
2017-01-27 10:57:14 -0500

    Revert "edit image sizes"
    This reverts commit 63d4fcfa8996c42483dd4f68fa38308110965168.

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,4 @@
-var jimp = require("gulp-jimp-resize");
 var gulp = require('gulp');
- 
-gulp.task('images', function() {
-    return gulp.src(
-        '_uploads/*.{png,jpg,bmp}'
-        )
-    .pipe(jimp({
-        sizes: [
-            {"width": 960},
-        ]
-    }))
-    .pipe(gulp.dest('_uploads/'));
-});
 
 gulp.task('default', function() {
   // place code for your default task here
